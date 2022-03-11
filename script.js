3d4d14d8b615c664559611c007272a823ab5ccbc
Maakyno <teddygoss1@hotmail.com>
2022-03-11 19:28:21 +0100

    intégration css contact

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//menu mobile
 var burger = document.getElementById('burger');
 var header = document.querySelector('.header');
 var links = document.querySelectorAll('.navbar__link');
@@ -11,6 +12,22 @@ burger.addEventListener('click', toggleNav)
 links.forEach(function(link){
     link.addEventListener('click', toggleNav)
 })
+
+var textarea = document.querySelector('textarea');
+
+textarea.addEventListener('keydown', autosize);
+
+function autosize() {
+    var el = this;
+    console.log(el.scrollHeight);
+    setTimeout(function(){
+        el.style.cssText = 'height:auto; padding: 0';
+        el.style.cssText = 'height:' + el.scrollHeight + 'px';
+    },0);
+}
+
+
+
 
 
 
