a48adcb7ab9ccc3b01d3d447fbd5934a0cc78a51
Maakyno <teddygoss1@hotmail.com>
2022-03-16 20:49:29 +0100

    modification js

--- script.js
+++ script.js
@@ -36,19 +36,16 @@ let observer = new IntersectionObserver(function (entries) {
 
     entries.forEach(function (entrie) {
         // si le seuil d'apparition est supérieur a 0.5 alors on affiche la section
-        if (entrie.intersectionRatio > 0.5) {
+        if (entrie.isIntersecting) {
             entrie.target.classList.remove('not-visible')
             observer.unobserve(entrie.target)
             console.log('item visible')
+            console.log(entrie)
         }
-
-
-
     })
 
-
 }, {
-    //quand le seuil d'appartition de l'élément atteint 0.5, l'événement est déclenché
+    //quand le seuil d'appartition de l'élément atteint 0.75, l'événement est déclenché.
     threshold: [0.75]
 })
 
